3279731ca9c6d42eebbac0a2a90a39c957f845bd
Pedro Camargo <c@margo.co>
2025-08-14 12:51:17 +1000

    test improvement

---
+++ conftest.py
@@ -0,0 +1,14 @@
+import os
+import pytest
+from tempfile import gettempdir
+
+
+
+def pytest_sessionstart(session):
+
+    directory = str(gettempdir())
+
+    if directory not in os.environ["PATH"]:
+        os.environ["PATH"] = directory + os.pathsep + os.environ["PATH"]
+    if "PROJ_LIB" not in os.environ:
+        os.environ["PROJ_LIB"] = directory
